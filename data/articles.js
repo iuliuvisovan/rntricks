3d5f17598431e5366122e84355504b3d1745f4a1
unknown <iuliuvisovan@gmail.com>
2020-04-15 00:01:55 +0300

    new article + fix typos

--- data/articles.js
+++ data/articles.js
@@ -2,5 +2,6 @@ module.exports = [
   require('./articles/1/data.js'), 
   require('./articles/2/data.js'), 
   require('./articles/3/data.js'), 
-  require('./articles/4/data.js')
+  require('./articles/4/data.js'),
+  require('./articles/5/data.js')
 ];
