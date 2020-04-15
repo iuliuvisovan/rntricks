f2667c8e2d93a0c4b6abe18abe7d9c392b3aadc8
unknown <iuliuvisovan@gmail.com>
2020-04-16 01:52:30 +0300

    hide keyboard answer done

--- data/articles/7/data.js
+++ data/articles/7/data.js
@@ -6,11 +6,11 @@ module.exports = {
   body: [
     {
       type: 'h2',
-      innerText: `1. Hiding The Keyboard in Response to A User Tap`,
+      innerText: `1. Hiding the keyboard in response to a user tap`,
     },
     {
       type: 'p',
-      innerText: `This method will allow the user to be able to hide the keyboard when tapping almost anywhere on the screen outside of your <code>TextInput</code>'s area.`,
+      innerText: `This method will allow the user to hide the keyboard when tapping anywhere outside of the <code>TextInput</code>'s area.`,
     },
     {
       type: 'p',
@@ -19,10 +19,25 @@ module.exports = {
     {
       type: 'pre',
       innerText: `
-  <ScrollView <b>keyboardDismissMode="always"</b>>
+  <ScrollView <b>keyboardDismissMode="on-drag"</b>>
     <TextInput />
-  </ScrollView>
-      `,
+  </ScrollView>`,
+    },
+    {
+      type: 'h2',
+      innerText: `2. Hiding the keyboard programatically `,
+    },
+    {
+      type: 'p',
+      innerText: `This allows the developer to hide the keyboard programatically from JS, whenever needed.`,
+    },
+    {
+      type: 'pre',
+      innerText: `
+  import { Keyboard } from 'react-native';
+
+  //anywhere
+  <b>Keyboard.dismiss();</b>`,
     },
   ],
 };
