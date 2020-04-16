88498e0f234646f2b2391c0304e2c75fbcb9315a
unknown <iuliuvisovan@gmail.com>
2020-04-17 01:35:35 +0300

    added compression

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var compression = require('compression')
 
 var app = express();
 
@@ -10,10 +11,13 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(compression())
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
 app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next) => {
