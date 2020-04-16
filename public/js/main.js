0dcb36e140b1a6167a96dcc77e6bfcebea005202
unknown <iuliuvisovan@gmail.com>
2020-04-17 00:58:07 +0300

    remove jquery and bootstrap

--- public/js/main.js
+++ public/js/main.js
@@ -1,96 +1,31 @@
-(function ($) {
-  'use strict';
-
-  // Fixed Nav
+document.addEventListener('readystatechange', () => {
   var lastScrollTop = 0;
-  $(window).on('scroll', function () {
-    var wScroll = $(this).scrollTop();
-    if (wScroll > $('#nav').height()) {
+
+  window.addEventListener('scroll', function () {
+    var wScroll = this.scrollY;
+    if (wScroll > document.querySelector('#nav').clientHeight) {
       if (wScroll < lastScrollTop) {
-        $('#nav-fixed').removeClass('slide-up').addClass('slide-down');
+        document.querySelector('#nav-fixed').classList.remove('slide-up');
+        document.querySelector('#nav-fixed').classList.add('slide-down');
       } else {
-        $('#nav-fixed').removeClass('slide-down').addClass('slide-up');
+        document.querySelector('#nav-fixed').classList.remove('slide-down');
+        document.querySelector('#nav-fixed').classList.add('slide-up');
       }
     }
     lastScrollTop = wScroll;
   });
 
-  // Search Nav
-  $('.search-btn').on('click', function () {
-    $('.search-form').addClass('active');
+  document.querySelector('.search-btn').addEventListener('click', () => {
+    document.querySelector('.search-form').classList.add('active');
     setTimeout(() => {
-      $('.search-input').focus();
+      document.querySelector('.search-input').focus();
     }, 100);
   });
 
-  $('.search-close').on('click', function () {
-    $('.search-form').removeClass('active');
+  document.querySelector('.search-close').addEventListener('click', () => {
+    document.querySelector('.search-form').classList.remove('active');
     if (window.location.href.includes('/search')) {
       window.location.href = '/';
     }
   });
-
-  // Aside Nav
-  $(document).click(function (event) {
-    if (!$(event.target).closest($('#nav-aside')).length) {
-      if ($('#nav-aside').hasClass('active')) {
-        $('#nav-aside').removeClass('active');
-        $('#nav').removeClass('shadow-active');
-      } else {
-        if ($(event.target).closest('.aside-btn').length) {
-          $('#nav-aside').addClass('active');
-          $('#nav').addClass('shadow-active');
-        }
-      }
-    }
-  });
-
-  $('.nav-aside-close').on('click', function () {
-    $('#nav-aside').removeClass('active');
-    $('#nav').removeClass('shadow-active');
-  });
-
-  // Sticky Shares
-  var $shares = $('.sticky-container .sticky-shares'),
-    $sharesHeight = $shares.height(),
-    $sharesTop,
-    $sharesCon = $('.sticky-container'),
-    $sharesConTop,
-    $sharesConleft,
-    $sharesConHeight,
-    $sharesConBottom,
-    $offsetTop = 80;
-
-  function setStickyPos() {
-    if ($shares.length > 0) {
-      $sharesTop = $shares.offset().top;
-      $sharesConTop = $sharesCon.offset().top;
-      $sharesConleft = $sharesCon.offset().left;
-      $sharesConHeight = $sharesCon.height();
-      $sharesConBottom = $sharesConHeight + $sharesConTop;
-    }
-  }
-
-  function stickyShares(wScroll) {
-    if ($shares.length > 0) {
-      if ($sharesConBottom - $sharesHeight - $offsetTop < wScroll) {
-        $shares.css({ position: 'absolute', top: $sharesConHeight - $sharesHeight, left: 0 });
-      } else if ($sharesTop < wScroll + $offsetTop) {
-        $shares.css({ position: 'fixed', top: $offsetTop, left: $sharesConleft });
-      } else {
-        $shares.css({ position: 'absolute', top: 0, left: 0 });
-      }
-    }
-  }
-
-  $(window).on('scroll', function () {
-    stickyShares($(this).scrollTop());
-  });
-
-  $(window).resize(function () {
-    setStickyPos();
-    stickyShares($(this).scrollTop());
-  });
-
-  setStickyPos();
-})(jQuery);
+});
