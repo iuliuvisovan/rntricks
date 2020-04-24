dff0c13f963bc9c300fbb05ef99575f192aee440
unknown <iuliuvisovan@gmail.com>
2020-04-24 18:55:45 +0300

    nodemon and stuff

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var compression = require('compression')
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const compression = require('compression');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(compression())
+app.use(compression());
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,7 +20,7 @@ app.use(cookieParser());
 
 app.use(express.static(__dirname + '/public'));
 
-app.use((req, res, next) => {
+app.use((req, _, next) => {
   req.url = req.url.replace(/[/]+/g, '/');
   req.originalUrl = req.originalUrl.replace(/[/]+/g, '/');
   next();
@@ -32,12 +32,12 @@ app.use('/category', require('./routes/category-router'));
 app.use('/search', require('./routes/search-router'));
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
